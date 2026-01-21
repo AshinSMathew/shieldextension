8c66ab55b90fe7dc49a3e7d011cfc0f081d09aa5
AshinSMathew <ashinmathew1010@gmail.com>
2026-01-21 11:39:03 +0530

    feat: new code

--- backend/main.py
+++ backend/main.py
@@ -1,44 +1,38 @@
 import os
-from socket import socket
 import psycopg2
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from dotenv import load_dotenv
 
 load_dotenv()
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-app = FastAPI(title="AuthToken API")
+app = FastAPI()
 
-class SessionIn(BaseModel):
+class TokenIn(BaseModel):
     auth_token: str
 
-def get_connection():
-    return psycopg2.connect(
-        DATABASE_URL,
-        sslmode="require",
-    )
-
 @app.post("/add")
-def add_session(data: SessionIn):
+def add_token(data: TokenIn):
     try:
-        conn = get_connection()
+        conn = psycopg2.connect(
+            DATABASE_URL,
+            sslmode="require"
+        )
         cur = conn.cursor()
 
         cur.execute(
-            """
-            INSERT INTO device_sessions (auth_token)
-            VALUES (%s)
-            """,
+            "INSERT INTO device_sessions (auth_token) VALUES (%s) RETURNING id;",
             (data.auth_token,)
         )
 
+        new_id = cur.fetchone()[0]
         conn.commit()
+
         cur.close()
         conn.close()
 
-        return {"message": "Auth token stored successfully"}
+        return {"id": new_id}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
